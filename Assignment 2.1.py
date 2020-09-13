844d031ce341d9ad962f0d02326f1f67bbd841a3
Sholdman2012 <66438020+Sholdman2012@users.noreply.github.com>
2020-09-13 16:43:03 -0500

    Create Assignment 2.1.py

---
+++ Assignment 2.1.py
@@ -0,0 +1,37 @@
+#Sarah Holdman
+#Assignment 2.1
+#9/13/2020
+
+# Display the text “Hello World! I wonder why that is always the default coding text to start with”
+print("Hello World! I wonder why that is always the default coding text to start with")
+
+
+# Add two numbers together
+print(100 + 32)
+
+# Subtract a number from another number
+print(54 - 29)
+
+# Multiply two numbers
+print(265 * 7849)
+
+# Divide between two numbers
+print(584 / 7)
+
+# Concatenate two strings together (any words)
+print("Hello" + "Sarah")
+
+# Create a list of 4 items (can be strings, numbers, both)
+my_list = [1, "Two", "potato", 7.5]
+print(my_list)
+print(type(my_list))
+
+# Append an item to your list (again, can be a string, number)
+my_list.append("tomato")
+print(my_list)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+my_tuple = ("ketchup", "mustard", 8, "mayo")
+print(my_tuple)
+print(type(my_tuple)) 
+
